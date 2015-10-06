d07f8e3310a098ea5b8618e29c63a9384aa228ef
and <and@sitemule.com>
2015-10-06 11:12:04 +0200

    error in --target-save-file and --source-save-til

--- src/AS400.py
+++ src/AS400.py
@@ -1,21 +1,23 @@
+import uuid
+
 class AS400:
 	def __init__(self, config):
 		self.source = config['source']
 		self.target = config['target']
+		self.save_file = None
 
 	def save(self):
 		# save_file is the file which the AS/400 library / object should be saved to
-		save_file = ('/tmp/zipSeries-' + str(uuid.uuid1()) + '.zs4') if self.target['save-file'] == '' else self.target['save-file'];
-		self.source['save-file'] = save_file
-	
+		self.save_file = ('/tmp/zipSeries-' + str(uuid.uuid1()) + '.zs4') if self.source['save-file'] == '' else self.source['save-file']
+		
 		# TODO Write FTP script
 
 		print self.source
 
-	def restore(self, file):
+	def restore(self, save_file=None):
 		# save_file is the saved file that should be restored on the AS/400
-		save_file = self.source['save-file']
-		
+		if save_file == None:
+			save_file = self.save_file
 		# TODO Write FTP script
 		
 		print self.target

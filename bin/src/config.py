65d7b61db5e9d4609e056832d956e6e51ad30f3f
Andreas Louv <andreas@louv.dk>
2016-04-11 15:18:14 +0200

    Moved src to bin

--- bin/src/config.py
+++ bin/src/config.py
@@ -1,9 +1,9 @@
 import os
 import json
 
-with open(os.path.dirname(os.path.realpath(__file__)) + '/../data/target_releases.json') as data_file:
+with open(os.path.dirname(os.path.realpath(__file__)) + '/../../data/target_releases.json') as data_file:
 	RELEASE_LIST = json.load(data_file)
-with open(os.path.dirname(os.path.realpath(__file__)) + '/../data/object_types.json') as data_file:
+with open(os.path.dirname(os.path.realpath(__file__)) + '/../../data/object_types.json') as data_file:
 	OBJECT_TYPE_LIST = json.load(data_file)
 
 UPGRADE_FROM = 'https://github.com/ginkoms/zipSeries'
